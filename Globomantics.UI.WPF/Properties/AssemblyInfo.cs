6fe0d518b82154b9c0ed4771e6611a90f26c002c
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-10-18 22:52:21 +0200

    Add project files.

---
+++ Globomantics.UI.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Globomantics.UI.WPF")]
+[assembly: AssemblyDescription("Globomantics UI WPF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Globomantics")]
+[assembly: AssemblyProduct("Globomantics UI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
