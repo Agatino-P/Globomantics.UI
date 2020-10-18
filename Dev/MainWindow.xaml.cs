609eb45229104f0c031031f5e489031ff626fa47
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-10-18 23:50:07 +0200

    Playing with xmlns assemblyinfo

---
+++ Dev/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Dev
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Globomantics.UI.WPF/Properties/AssemblyInfo.cs
+++ Globomantics.UI.WPF/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("Globomantics.UI.WPF")]
 [assembly: AssemblyDescription("Globomantics UI WPF")]
@@ -12,6 +13,8 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://globomantics.com/2019/ui/wpf", "Globomantics.UI.WPF")]
+[assembly: XmlnsPrefix("http://globomantics.com/2019/ui/wpf", "globo")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
